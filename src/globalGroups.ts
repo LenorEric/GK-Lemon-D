73339bb3eef0016ed747ce8aca937c126840003a
Lenor Tang <40317566+LenorEric@users.noreply.github.com>
2026-01-13 14:25:09 +0800

    remove global group

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -6,19 +6,5 @@ export const UPDATE_PROMPT_ORDER = -9; // 更新提示
 export const YOUTH_MODE_ORDER = -8; // 青少年模式
 
 export default defineGkdGlobalGroups([
-  {
-    key: 0,
-    name: '开屏广告-全局',
-    desc: '关闭打开应用时的开屏广告',
-    order: OPEN_AD_ORDER,
-    fastQuery: true,
-    matchTime: 10000,
-    actionMaximum: 2,
-    resetMatch: 'app',
-    forcedTime: 10000,
-    actionCdKey: 0,
-    actionMaximumKey: 0,
-    priorityTime: 10000,
-    disableIfAppGroupMatch: '开屏广告',
-    rules: [
+
 ]);
