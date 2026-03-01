56991ac35833f328dd983bfc5d6532b2db20ef34
Lenor Tang <40317566+LenorEric@users.noreply.github.com>
2026-03-01 20:45:07 +0800

    Create com.xingin.xhs.ts

---
+++ src/apps/com.xingin.xhs.ts
@@ -0,0 +1,28 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+    id: 'com.xingin.xhs',
+    name: '小红书',
+    groups: [
+        {
+            key: 0,
+            name: '权限提示-通知权限',
+            fastQuery: true,
+            actionMaximum: 1,
+            resetMatch: 'app',
+            rules: [
+                {
+                    key: 0,
+                    activityIds: [
+                        '.index.v2.IndexActivityV2',
+                        '.notification.NotificationAuthorizationTranslucentActivity',
+                        'com.xingin.matrix.notedetail.r10.comment.r10.NoteCommentActivity',
+                        'com.xingin.authorization.NotificationAuthorizationTranslucentActivity',
+                    ],
+                    matches:
+                        '@ImageView[clickable=true][visibleToUser=true] - LinearLayout > [text*="通知"]',
+                },
+            ],
+        },
+    ],
+});
