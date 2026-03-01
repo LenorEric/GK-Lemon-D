dce022becbd5b9c4c1abcb38f5532ca45e4117b6
Lenor Tang <40317566+LenorEric@users.noreply.github.com>
2026-03-01 20:16:14 +0800

    Update globalGroups.ts

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -4,6 +4,7 @@ import * as appList from './globalDefaultApps';
 export const OPEN_AD_ORDER = -10; // 开屏广告
 export const UPDATE_PROMPT_ORDER = -9; // 更新提示
 export const YOUTH_MODE_ORDER = -8; // 青少年模式
+export const POPUP_AD_ORDER = -7; // 弹窗广告
 
 export default defineGkdGlobalGroups([
   {
@@ -156,5 +157,21 @@ export default defineGkdGlobalGroups([
       .concat(
         [...appList.yongWhiteListAppIDs].map((id) => ({ id, enable: true })),
       ),
+  },
+  {
+    key: 3,
+    name: '弹窗广告-全局',
+    desc: '关闭应用的弹窗广告',
+    order: POPUP_AD_ORDER,
+    fastQuery: true,
+    disableIfAppGroupMatch: '弹窗广告',
+    rules: [
+      {
+        key: 0,
+        matches: [
+          '[vid="ad_close"][clickable=true]',
+        ],
+      },
+    ],
   },
 ]);
