b4a7794874b4a41d03ac4885d4af36345ea66220
Lenor Tang <40317566+LenorEric@users.noreply.github.com>
2026-07-31 10:29:25 +0800

    Update com.tencent.wework.ts

--- src/apps/com.tencent.wework.ts
+++ src/apps/com.tencent.wework.ts
@@ -30,7 +30,21 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          matches: '[text="进入"][vid="ele"]',
+          matches: '[text="进入"]',
+          fastQuery: true,
+          actionMaximum: 1,
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '功能类-自动同意软件协议',
+      activityIds: ['com.tencent.wework.login.controller.LoginWxAuthActivity'],
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[text="请阅读并同意《软件许可及服务协议》和《隐私政策》"] <<2 * + * > [text="同意"]',
           fastQuery: true,
           actionMaximum: 1,
         },
